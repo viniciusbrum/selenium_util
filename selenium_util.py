4e660fd95fc3b91b01d66bb2563a91ea503f9a85
Vinicius Brum <vinicampbrum@gmail.com>
2018-09-05 12:38:19 -0300

    Importa conjunto de tipos de seletores disponíveis

--- selenium_util.py
+++ selenium_util.py
@@ -5,6 +5,7 @@ from abc import ABCMeta
 from abc import abstractmethod
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import Select
